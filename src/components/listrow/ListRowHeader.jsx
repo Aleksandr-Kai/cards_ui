608ca08a8ba37ce70c225369b6520dcc642857ba
A.Timofeev <timofeev.aleksandr.mail@gmail.com>
2023-10-04 23:01:00 +0300

    rework list edit decor

--- src/components/listrow/ListRowHeader.jsx
+++ src/components/listrow/ListRowHeader.jsx
@@ -4,32 +4,32 @@ import classesEx from "./celldecor.module.css";
 import CellEx from "../cell/CellEx";
 
 function ListRowHeader({ className }) {
-    return (
-        <div className={classNames(className, classes.row)}>
-            <CellEx
-                className={classNames(classes.cell, classesEx.decorHeader)}
-                valueType={"statictext"}
-                value={"Word"}
-            />
-            <CellEx
-                className={classNames(classes.cell, classesEx.decorHeader)}
-                valueType={"statictext"}
-                value={"Translation"}
-            />
-            <CellEx
-                className={classNames(classes.boolcell, classesEx.decorHeader)}
-                valueType="statictext"
-                value={"St"}
-                title={"Word status"}
-            />
-            <CellEx
-                className={classNames(classes.boolcell, classesEx.decorHeader)}
-                valueType="statictext"
-                value={"RM"}
-                title={"Delete or create word position"}
-            />
-        </div>
-    );
+	return (
+		<div className={classNames(className, classes.row)}>
+			<CellEx
+				className={classNames(classes.cell, classesEx.decorHeader)}
+				valueType={"statictext"}
+				value={"Word"}
+			/>
+			<CellEx
+				className={classNames(classes.cell, classesEx.decorHeader)}
+				valueType={"statictext"}
+				value={"Translation"}
+			/>
+			<CellEx
+				className={classNames(classes.boolcell, classesEx.decorHeader)}
+				valueType="statictext"
+				value={"☑"}
+				title={"Word status"}
+			/>
+			<CellEx
+				className={classNames(classes.boolcell, classesEx.decorHeader)}
+				valueType="statictext"
+				value={"⇌"}
+				title={"Delete or create word position"}
+			/>
+		</div>
+	);
 }
 
 export default ListRowHeader;
